c1ff66f7de9ec864149da3d64e5530a2ad7e2a88
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-07-11 19:23:27 -0700

    made a click event listener that equates the value of the input field to the city variable declared globally and then inputs that into the query url to retrieve weather data for that city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
 var APIKey = 'a9a3664e3b7b37a690bb362ee24e7e9f';
-var city = 'atlanta';
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var city ;
+var searchInput = document.getElementById('search-input');
+var searchButton = document.getElementById('search-button');
 
-fetch(queryURL)
+searchButton.addEventListener('click', function() {
+    city = searchInput.value;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+    fetch(queryURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
   });
+
+})
+
