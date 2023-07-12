3aaa20ea2bca812aa659b5659c51317af14b0ce8
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-07-11 21:31:41 -0700

    put the fetch and display into one function so i can call on it on both the added event listeners one for the sumbit for user input and one for the buttons generated

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,38 +7,50 @@ var singleDayDisplay = document.getElementById('single-day-display');
 
 var city ;
 
-searchButton.addEventListener('click', function() {
-    city = searchInput.value;
+function fetchAndDisplayWeather(city) {
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
 
     fetch(queryURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    singleDayDisplay.innerHTML = "";
-    
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            singleDayDisplay.innerHTML = "";
+
+            var cityDisplay = document.createElement('div');
+            var cityName = document.createElement('h5');
+            var currentTemp = document.createElement('p');
+            var minTemp = document.createElement('p');
+            var maxTemp = document.createElement('p');
+
+            cityName.textContent = city;
+            currentTemp.textContent = "Current Temp: " + data.main.temp + "\u00B0F";
+            minTemp.textContent = "Min Temp: " + data.main.temp_min + "\u00B0F";
+            maxTemp.textContent = "Max Temp: " + data.main.temp_max + "\u00B0F";
+
+            cityDisplay.append(cityName, currentTemp, minTemp, maxTemp);
+            singleDayDisplay.append(cityDisplay);
+
+            console.log(data);
+        });
+}
+
+searchButton.addEventListener('click', function() {
+    city = searchInput.value;
+    fetchAndDisplayWeather(city);
+
     var searchedItems = document.createElement('button');
-    var cityDisplay = document.createElement('div');
-    var cityName = document.createElement('h5');
-    var currentTemp = document.createElement('p');
-    var minTemp = document.createElement('p');
-    var maxTemp = document.createElement('p');
-
     searchedItems.textContent = city;
-    cityName.textContent = city;
-    currentTemp.textContent = "Current Temp: " + data.main.temp + "\u00B0F";
-    minTemp.textContent = "Min Temp: " + data.main.temp_min + "\u00B0F";
-    maxTemp.textContent = "Max Temp: " + data.main.temp_max + "\u00B0F";
-
     displaySearch.append(searchedItems);
-    cityDisplay.append(cityName, currentTemp, minTemp, maxTemp);
-    singleDayDisplay.append(cityDisplay);
+    searchedItems.addEventListener('click', function() {
+        fetchAndDisplayWeather(city);
+    });
+});
 
-
-
-    console.log(data);
-  });
-
-})
+displaySearch.addEventListener('click', function(event) {
+    if (event.target.tagName === 'BUTTON') {
+        var clickedCity = event.target.textContent;
+        fetchAndDisplayWeather(clickedCity);
+    }
+});
 
